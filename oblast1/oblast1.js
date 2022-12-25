74a70777f8b9e29bb64381eb8284fe9de9768c28
Zmici567 <zmilica.567@gmail.com>
2022-12-25 14:51:29 +0100

    aaa

--- oblast1/oblast1.js
+++ oblast1/oblast1.js
@@ -1,3 +1,29 @@
-<script>
-  function validateAndSubmit() {}
-</script>
+let bodovi=0;
+let BRpokusaja=0;
+
+class pitanje
+{
+  constructor(textPitanja, odgovorA, odgovorB, odgovorC,TacanOdgovor)
+  {
+    this.textPitanja=textPitanja;
+    this.odgovor=[odgovorA,odgovorB,odgovorC];
+    this.TacanOdgovor= TacanOdgovor;
+  } 
+}
+
+let pitanja=[];
+pitanja[0]=new pitanje("Dobili ste vrlo gadne poruke preko Instagrama, korisnik naloga je nepoznat, i uspešno krije identitet.","A - Pročitajte ostatak poruka","B - Odmah odgovorite, ignorišite ostale poruke","C - Ignorišite ostatak poruka odmah",0)
+
+function optionChoose(text,rb)
+{
+  if(text===pitanja[rb].odgovor[pitanja[rb].TacanOdgovor])
+  {
+    bodovi+=(2-BRpokusaja);
+    BRpokusaja=0;
+  }
+  else if(BRpokusaja==0)
+  {
+     BRpokusaja++;
+     document.getElementById("pomoc").style.display="flex";
+  }
+}
